d0865380e56699a605239855b9d4b852e936253c
Chechu <22799163+DarkDragon117@users.noreply.github.com>
2022-02-21 21:40:28 +0100

    Added a way to choose whether to map all the commands below  the specified one or not.

--- core/src/main/java/dev/chechu/core/commands/HelpManager.java
+++ core/src/main/java/dev/chechu/core/commands/HelpManager.java
@@ -1,21 +1,33 @@
 package dev.chechu.core.commands;
 
-public abstract class HelpManager {
-    private String mainCommand;
+import org.jetbrains.annotations.Nullable;
 
-    public HelpManager(String mainCommand) {
+public abstract class HelpManager {
+    private final String mainCommand;
+    private final CommandManager commandManager;
+
+    public HelpManager(String mainCommand, CommandManager commandManager) {
         this.mainCommand = "/" + mainCommand;
+        this.commandManager = commandManager;
     }
 
-    public void getHelp(Command command) {
+    /**
+     * Gets help from the specified command, if specified it also maps the commands under it.
+     * @param command Command from which to extract the help.
+     * @param mapUnder Whether to map the commands under the specified command or not.
+     * @return The help from the specified command.
+     */
+    public String getHelp(Command command, boolean mapUnder) {
         StringBuilder help = new StringBuilder();
         help.append(mainCommand + " ")
                 .append(command.getDescription().getCommand()).append(" ")
                 .append(getParams(command)).append("- ")
                 .append(command.getDescription());
-        for (Command subcommand : command.getSubcommands()) {
-            help.append("\n")
-                    .append(getHelp(subcommand));
+        if (mapUnder) {
+            for (Command subcommand : command.getDescription().getSubcommands()) {
+                help.append("\n")
+                        .append(getHelp(subcommand, true));
+            }
         }
         help.append("---");
         return String.valueOf(help);
@@ -28,7 +40,7 @@ public abstract class HelpManager {
      */
     public String getParams(Command command) {
         StringBuilder params = new StringBuilder();
-        for (String param : command.getParams()) {
+        for (String param : command.getDescription().getParams()) {
             String parameter;
             if (param.startsWith("%o")) parameter = "["+param.substring(1)+"]";
             else parameter = "<"+param+">";
@@ -43,8 +55,8 @@ public abstract class HelpManager {
      */
     public String getAllHelp() {
         StringBuilder help = new StringBuilder();
-        for (Command command : getCommands()) {
-            help.append(getHelp(command)).append("\n");
+        for (Command command : commandManager.getCommandList()) {
+            help.append(getHelp(command, true)).append("\n");
         }
         return String.valueOf(help);
     }
