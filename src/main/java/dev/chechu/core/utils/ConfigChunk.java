2aee33efd45a8f0757754cdeded6e593a70b8f8b
Chechu <22799163+DarkDragon117@users.noreply.github.com>
2022-02-25 22:01:47 +0100

    Created a ConfigChunk object in order to save values, keys, default values and fixable values.

---
+++ src/main/java/dev/chechu/core/utils/ConfigChunk.java
@@ -0,0 +1,31 @@
+package dev.chechu.core.utils;
+
+import lombok.*;
+
+@Getter
+public class ConfigChunk<T> {
+    private final String key;
+    private final T defaultValue;
+    @Setter private T value;
+    private final Class<T> type;
+    private String oldKey = null;
+
+    public ConfigChunk(String key, T defaultValue) {
+        this.type = (Class<T>) defaultValue.getClass();
+        this.defaultValue = defaultValue;
+        this.key = key;
+        this.value = defaultValue;
+    }
+
+    public ConfigChunk(String key, T defaultValue, String oldKey) {
+        this.type = (Class<T>) defaultValue.getClass();
+        this.defaultValue = defaultValue;
+        this.key = key;
+        this.value = defaultValue;
+        this.oldKey = oldKey;
+    }
+
+    public boolean isFixable() {
+        return oldKey != null;
+    }
+}
