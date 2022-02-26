0cf3b8cda90559f57b975c6a670f4bfe11d4f0b7
Chechu <22799163+DarkDragon117@users.noreply.github.com>
2022-02-26 15:55:25 +0100

    Finished Spigot Updater, pending test.

--- src/main/java/dev/chechu/spigot/SpigotUpdater.java
+++ src/main/java/dev/chechu/spigot/SpigotUpdater.java
@@ -2,26 +2,54 @@ package dev.chechu.spigot;
 
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
+import dev.chechu.core.Core;
 import dev.chechu.core.Updater;
+import dev.chechu.core.utils.Message;
+import org.bukkit.plugin.InvalidPluginException;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.PluginLoader;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
+import java.nio.channels.Channels;
+import java.nio.channels.FileChannel;
+import java.nio.channels.ReadableByteChannel;
 import java.util.logging.Logger;
+import org.apache.commons.io.FilenameUtils;
 
 public class SpigotUpdater extends Updater {
     private final String USER_AGENT  = "DragonAPI";// Change this!
     private final String REQUEST_URL;
+    private final String DOWNLOAD_URL;
+    private final File PLUGIN_FOLDER;
     private final Logger logger;
+    private final JavaPlugin plugin;
     public SpigotUpdater(JavaPlugin plugin, String resourceID, boolean autoUpdate) {
+        this.plugin = plugin;
         setAutoUpdateEnabled(autoUpdate);
         this.logger = plugin.getLogger();
         REQUEST_URL = "https://api.spiget.org/v2/resources/"+resourceID+"/versions/latest";
+        DOWNLOAD_URL = "https://api.spiget.org/v2/resources/"+resourceID+"/download";
+        PLUGIN_FOLDER = plugin.getDataFolder().getParentFile();
         setCurrentVersion(plugin.getDescription().getVersion());
-        setLatestVersion(getLatestVersion());
+    }
+
+    @Override
+    public void tryUpdate() {
+        if(checkForNewVersion()) {
+            if (this.isAutoUpdateEnabled()) {
+                logger.info(Message.get("update_available_auto"));
+                if(downloadNewVersion()) logger.info(Message.get("update_success"));
+                else logger.info(Message.get("update_fail"));
+            } else {
+                logger.warning(Message.get("update_available_no_auto"));
+            }
+        }
     }
 
     @Override
@@ -35,18 +63,13 @@ public class SpigotUpdater extends Updater {
 
     @Override
     public boolean checkForNewVersion() {
+        logger.info(Message.get("update_search"));
+        setLatestVersion(getLatestVersion());
         if (getCurrentMajor() < getLatestMajor()) {
-            logger.warning("You're using an outdated version of the plugin!! The plugin will NOT try to update it, please update it manually.");
+            logger.warning(Message.get("update_major_difference"));
             return false;
         }
-        if (getCurrentMinor() < getLatestMinor()) {
-            if(this.isAutoUpdateEnabled()) {
-                logger.info("A new update has been detected, the plugin will try to update it automatically");
-            } else {
-                logger.warning("A new update has been detected but you have disabled autoUpdates");
-            }
-        }
-        return false;
+        return getCurrentMinor() < getLatestMinor() || getCurrentPatch() < getLatestPatch();
     }
 
     @Override
@@ -67,8 +90,10 @@ public class SpigotUpdater extends Updater {
                 return element.getAsJsonObject().get("name").getAsString();
             }
         } catch (IOException e) {
-            // TODO: handle exception
-            e.printStackTrace();
+            logger.severe(Message.get("update_version_fail"));
+            if (Core.debugMode) {
+                e.printStackTrace();
+            }
         }
         return "-1.-1.-1";
     }
@@ -83,7 +108,33 @@ public class SpigotUpdater extends Updater {
     }
 
     @Override
-    public void downloadNewVersion() {
+    public boolean downloadNewVersion() {
+        File oldPlugin;
+        File newPlugin;
+        PluginLoader loader = plugin.getPluginLoader();
+        loader.disablePlugin(plugin);
+        try {
+            newPlugin = new File(PLUGIN_FOLDER, FilenameUtils.getName((new URL(DOWNLOAD_URL)).getPath()));
+            Method getFileMethod = JavaPlugin.class.getDeclaredMethod("getFile");
+            getFileMethod.setAccessible(true);
+            oldPlugin = (File) getFileMethod.invoke(plugin);
+        } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException | MalformedURLException e) {
+            if (Core.debugMode)
+                e.printStackTrace();
+            return false;
+        }
 
+        if(!downloadFile(DOWNLOAD_URL,newPlugin,oldPlugin)) return false;
+
+        try {
+            Plugin thePlugin = loader.loadPlugin(newPlugin);
+            loader.enablePlugin(thePlugin);
+        } catch (InvalidPluginException e) {
+            if(Core.debugMode)
+                e.printStackTrace();
+        }
+
+
+        return true;
     }
 }
