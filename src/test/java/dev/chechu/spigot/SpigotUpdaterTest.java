8ded899088fd8f6244ab6779ff37f5ada0388684
Chechu <22799163+DarkDragon117@users.noreply.github.com>
2022-02-26 13:01:03 +0100

    Created a Spigot Updater Tester

---
+++ src/test/java/dev/chechu/spigot/SpigotUpdaterTest.java
@@ -0,0 +1,50 @@
+package dev.chechu.spigot;
+
+import be.seeseemelk.mockbukkit.MockBukkit;
+import dev.chechu.core.Updater;
+import org.bukkit.Server;
+import org.bukkit.plugin.PluginDescriptionFile;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SpigotUpdaterTest {
+    private static MockPlugin plugin;
+    private static PluginDescriptionFile pdf;
+    private static Server server;
+
+
+    @BeforeAll
+    public static void setUp() {
+        server = MockBukkit.mock();
+    }
+
+    @Test
+    public void assertThatNoNewVersionsExist() {
+        plugin = MockBukkit.loadWith(MockPlugin.class,new PluginDescriptionFile("MockPlugin","2.1.1","dev.chechu.spigot.MockPlugin"));
+        Updater updater = new SpigotUpdater(plugin, "63607");
+        assertFalse(updater.checkForNewVersion());
+    }
+
+    @Test
+    public void assertThatNewVersionExist() {
+        plugin = MockBukkit.loadWith(MockPlugin.class,new PluginDescriptionFile("MockPlugin","2.1.0","dev.chechu.spigot.MockPlugin"));
+        Updater updater = new SpigotUpdater(plugin, "63607");
+        assertTrue(updater.checkForNewVersion());
+    }
+
+    @Test
+    public void assertThatNewerVersionsArentDetected() {
+        plugin = MockBukkit.loadWith(MockPlugin.class,new PluginDescriptionFile("MockPlugin","2.1.2","dev.chechu.spigot.MockPlugin"));
+        Updater updater = new SpigotUpdater(plugin, "63607");
+        assertFalse(updater.checkForNewVersion());
+    }
+
+    @AfterAll
+    public static void tearDown() {
+        MockBukkit.unmock();
+    }
+}
