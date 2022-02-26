6e162903ee4763085d33fc6dbe82d97445f71c94
Chechu <22799163+DarkDragon117@users.noreply.github.com>
2022-02-26 12:59:56 +0100

    Created a Bukkit Mock Plugin to test the updater

---
+++ src/test/java/dev/chechu/spigot/MockPlugin.java
@@ -0,0 +1,19 @@
+package dev.chechu.spigot;
+
+import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.plugin.java.JavaPluginLoader;
+
+import java.io.File;
+
+public class MockPlugin extends JavaPlugin {
+    public MockPlugin()
+    {
+        super();
+    }
+
+    protected MockPlugin(JavaPluginLoader loader, PluginDescriptionFile description, File dataFolder, File file)
+    {
+        super(loader, description, dataFolder, file);
+    }
+}
