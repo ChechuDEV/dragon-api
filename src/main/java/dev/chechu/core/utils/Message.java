710f69a39b5d36c9846de01107a880a3ef34e250
Chechu <22799163+DarkDragon117@users.noreply.github.com>
2022-02-26 15:52:29 +0100

    Created a Messages file

---
+++ src/main/java/dev/chechu/core/utils/Message.java
@@ -0,0 +1,29 @@
+package dev.chechu.core.utils;
+
+import org.yaml.snakeyaml.Yaml;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Message {
+    private static Map<String, Object> data;
+    public Message() {
+        Yaml yaml = new Yaml();
+        try {
+            InputStream in = getClass().getResourceAsStream("/langs/en_US.yml");
+            data = yaml.load(in);
+        } catch (Exception e) {
+            e.printStackTrace();
+            data = new HashMap<>();
+        }
+    }
+    public static String get(String key) {
+        Object obj = data.getOrDefault(key,"LANGUAGE ERROR");
+        if (obj instanceof String)
+            return (String) obj;
+        return "LANGUAGE ERROR";
+    }
+}
