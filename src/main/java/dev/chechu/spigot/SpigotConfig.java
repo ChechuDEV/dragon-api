5b080a8a765d35b2bb1ec2ea6d4224d92d9cb4e9
Chechu <22799163+DarkDragon117@users.noreply.github.com>
2022-02-26 15:53:11 +0100

    Added old config removal and deleted config version.

--- src/main/java/dev/chechu/spigot/SpigotConfig.java
+++ src/main/java/dev/chechu/spigot/SpigotConfig.java
@@ -2,10 +2,14 @@ package dev.chechu.spigot;
 
 import dev.chechu.core.Configuration;
 import dev.chechu.core.utils.ConfigChunk;
+import dev.chechu.core.utils.Message;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.Setter;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Logger;
@@ -13,47 +17,45 @@ import java.util.logging.Logger;
 public abstract class SpigotConfig implements Configuration {
     @Getter private final JavaPlugin plugin;
     @Getter private final Logger logger;
-    private FileConfiguration config;
+    @Getter(AccessLevel.PROTECTED) @Setter(AccessLevel.PROTECTED) private FileConfiguration config;
 
-    private final List<ConfigChunk<?>> configChunks = new ArrayList<>();
-
-    private final String currentVersion = "0.1";
-    private String fileVersion = "0.1";
+    @Getter(AccessLevel.PROTECTED) private final List<ConfigChunk<?>> configChunks = new ArrayList<>();
 
     public SpigotConfig(JavaPlugin plugin) {
         this.plugin = plugin;
         logger = plugin.getLogger();
         config = plugin.getConfig();
         createFiles();
-        if(fixVersions()) logger.info("Configuration has been fixed for you magically!");
+        if(fixVersions()) logger.info(Message.get("config_fix"));
     }
 
     @Override
     public void createFiles() {
         if (!plugin.getDataFolder().exists()) if(!plugin.getDataFolder().mkdir()) {
-            logger.severe("The plugin's folder couldn't have been created. Perhaps some permissions are missing?");
+            logger.severe(Message.get("config_folder_creation_error"));
         }
         plugin.saveDefaultConfig();
+        File configFile = new File(plugin.getDataFolder() + "config.yml");
+        if (!configFile.exists()) logger.severe(Message.get("config_file_creation_error"));
     }
 
     @Override
     public boolean fixVersions() {
-        fileVersion = config.getString("version","-1");
-        if(fileVersion.equals("-1")) {
-            logger.warning("The configuration file may be too old or corrupted in order to infer its version. " +
-                    "The plugin won't try to fix it and will try to read it anyways (Default values may be used).");
-            return false;
-        }
         boolean fixed = false;
         for (ConfigChunk<?> configChunk : configChunks) {
-            if(configChunk.isFixable()) {
-                if(!doesChunkExist(configChunk)) {
-                    config.set(configChunk.getKey(),configChunk.getDefaultValue());
+            if (configChunk.isFixable()) {
+                if (!doesChunkExist(configChunk)) {
+                    config.set(configChunk.getKey(), config.get(configChunk.getOldKey(),configChunk.getDefaultValue()));
+                    fixed = true;
+                }
+                if (config.get(configChunk.getOldKey(),null) != null) {
+                    config.set(configChunk.getOldKey(), null);
                     fixed = true;
                 }
             }
             setObjectFromChunk(configChunk);
         }
+
         if(fixed) plugin.saveConfig();
         return fixed;
     }
