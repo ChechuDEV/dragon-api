7071b0594f0803bae12225f28d53cbb9a9b33d52
Chechu <22799163+DarkDragon117@users.noreply.github.com>
2022-02-26 15:55:42 +0100

    Added a default downloader.

--- src/main/java/dev/chechu/core/Updater.java
+++ src/main/java/dev/chechu/core/Updater.java
@@ -2,6 +2,12 @@ package dev.chechu.core;
 
 import lombok.*;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
 import java.util.logging.Logger;
 
 public abstract class Updater {
@@ -18,13 +24,33 @@ public abstract class Updater {
 
     @Getter(AccessLevel.PROTECTED) @Setter(AccessLevel.PROTECTED) private Logger logger;
 
+    public abstract void tryUpdate();
+
     public abstract void setCurrentVersion(String version);
     public abstract String getLatestVersion();
     public abstract void setLatestVersion(String version);
 
     public abstract boolean checkForNewVersion();
 
-    public abstract void downloadNewVersion();
+    public abstract boolean downloadNewVersion();
 
-
+    public boolean downloadFile(String urlString, File file, File oldFile) {
+        try {
+            URL url = new URL(urlString);
+            ReadableByteChannel readableByteChannel = Channels.newChannel(url.openStream());
+            FileOutputStream fileOutputStream = new FileOutputStream(file);
+            fileOutputStream.getChannel().transferFrom(readableByteChannel,0,Long.MAX_VALUE);
+        } catch (IOException e) {
+            if (Core.debugMode)
+                e.printStackTrace();
+            return false;
+        }
+        if (oldFile != null) {
+            if (!oldFile.renameTo(new File(oldFile.getParentFile(),oldFile.getName()+".old"))) {
+                file.delete();
+                return false;
+            }
+        }
+        return true;
+    }
 }
