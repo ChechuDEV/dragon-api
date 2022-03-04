f52c8426919be485244c3258f03fe3bb373061e9
Chechu <22799163+DarkDragon117@users.noreply.github.com>
2022-03-05 00:17:50 +0100

    Made Configuration an abstract class instead of an interface to force some variables.

--- src/main/java/dev/chechu/core/Configuration.java
+++ src/main/java/dev/chechu/core/Configuration.java
@@ -1,48 +1,55 @@
 package dev.chechu.core;
 
 import dev.chechu.core.utils.ConfigChunk;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Logger;
 
-public interface Configuration {
+public abstract class Configuration {
+    @Getter(AccessLevel.PROTECTED) private final List<ConfigChunk<?>> configChunks = new ArrayList<>();
+    @Getter @Setter(AccessLevel.PROTECTED) private Logger logger;
 
     /**
      * Initializes configuration, by checking, creating and fixing files and lastly reloading the config.
      */
-    void initializeConfig();
+    public abstract void initializeConfig();
 
     /**
      * Reload all the config
      */
-    void reloadConfig();
+    public abstract void reloadConfig();
 
     //TODO: Parameters
     /**
      * Creates a specified file
      */
-    void createFiles();
+    public abstract void createFiles();
 
     /**
      * Fixes configuration versions.
      */
-    boolean fixVersions();
+    public abstract boolean fixVersions();
 
     /**
      * Sets the object to it value or default value.
      * @param chunk Chunk from which to extract the object, key and default value.
      * @param <T> Type of chunk.
      */
-    <T> void setObjectFromChunk(ConfigChunk<T> chunk);
+    public abstract <T> void setObjectFromChunk(ConfigChunk<T> chunk);
 
-    <T> boolean doesChunkExist(ConfigChunk<T> chunk);
+    public abstract <T> boolean doesChunkExist(ConfigChunk<T> chunk);
 
     /**
      * Checks if a file exists
      * @param file File to check
      * @return Whether the file exists
      */
-    default boolean checkFile(File file) {
+    public boolean checkFile(File file) {
         return file.exists();
     }
 }
