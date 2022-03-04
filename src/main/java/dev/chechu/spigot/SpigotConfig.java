dbcabf9ab267107a98f64e73445d7d830d4e982c
Chechu <22799163+DarkDragon117@users.noreply.github.com>
2022-03-05 00:18:21 +0100

    Moved some variables to Configuration to force common variables.

--- src/main/java/dev/chechu/spigot/SpigotConfig.java
+++ src/main/java/dev/chechu/spigot/SpigotConfig.java
@@ -10,39 +10,34 @@ import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.logging.Logger;
 
-public abstract class SpigotConfig implements Configuration {
+public abstract class SpigotConfig extends Configuration {
     @Getter private final JavaPlugin plugin;
-    @Getter private final Logger logger;
     @Getter(AccessLevel.PROTECTED) @Setter(AccessLevel.PROTECTED) private FileConfiguration config;
 
-    @Getter(AccessLevel.PROTECTED) private final List<ConfigChunk<?>> configChunks = new ArrayList<>();
 
     public SpigotConfig(JavaPlugin plugin) {
         this.plugin = plugin;
-        logger = plugin.getLogger();
+        setLogger(plugin.getLogger());
         config = plugin.getConfig();
         createFiles();
-        if(fixVersions()) logger.info(Message.get("config_fix"));
+        if(fixVersions()) getLogger().info(Message.get("config_fix"));
     }
 
     @Override
     public void createFiles() {
         if (!plugin.getDataFolder().exists()) if(!plugin.getDataFolder().mkdir()) {
-            logger.severe(Message.get("config_folder_creation_error"));
+            getLogger().severe(Message.get("config_folder_creation_error"));
         }
         plugin.saveDefaultConfig();
         File configFile = new File(plugin.getDataFolder() + "config.yml");
-        if (!configFile.exists()) logger.severe(Message.get("config_file_creation_error"));
+        if (!configFile.exists()) getLogger().severe(Message.get("config_file_creation_error"));
     }
 
     @Override
     public boolean fixVersions() {
         boolean fixed = false;
-        for (ConfigChunk<?> configChunk : configChunks) {
+        for (ConfigChunk<?> configChunk : getConfigChunks()) {
             if (configChunk.isFixable()) {
                 if (!doesChunkExist(configChunk)) {
                     config.set(configChunk.getKey(), config.get(configChunk.getOldKey(),configChunk.getDefaultValue()));
