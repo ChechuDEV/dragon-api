ad2447b5f5aa50c24103b4a542fe4ea778d41535
Chechu <22799163+DarkDragon117@users.noreply.github.com>
2022-03-13 01:05:00 +0100

    Added a FileBackup and multiple customisable actions to reload config (useful?). Added addToConfigChunks method. Added a generic card to Configuration in order to mark the plugin class (i.e. JavaPlugin for Bukkit)

--- src/main/java/dev/chechu/dragonapi/core/Configuration.java
+++ src/main/java/dev/chechu/dragonapi/core/Configuration.java
@@ -1,18 +1,27 @@
 package dev.chechu.dragonapi.core;
 
 import dev.chechu.dragonapi.core.utils.ConfigChunk;
+import dev.chechu.dragonapi.core.utils.Message;
 import lombok.AccessLevel;
 import lombok.Getter;
-import lombok.Setter;
 
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Logger;
 
-public abstract class Configuration {
+public abstract class Configuration<T> {
     @Getter(AccessLevel.PROTECTED) private final List<ConfigChunk<?>> configChunks = new ArrayList<>();
-    @Getter @Setter(AccessLevel.PROTECTED) private Logger logger;
+    @Getter private final Logger logger;
+    @Getter private final T plugin;
+    public Configuration(Logger logger, T plugin) {
+        this.logger = logger;
+        this.plugin = plugin;
+        createFiles();
+        initializeConfig();
+        setConfigChunks();
+        if(fixVersions()) getLogger().info(Message.get("config_fix"));
+    }
 
     /**
      * Initializes configuration, by checking, creating and fixing files and lastly reloading the config.
@@ -22,7 +31,15 @@ public abstract class Configuration {
     /**
      * Reload all the config
      */
-    public abstract void reloadConfig();
+    public void reloadConfig() {
+        extraReloadConfig();
+        finishReloadConfig();
+        setConfigChunks();
+    }
+
+    public abstract void finishReloadConfig();
+
+    public abstract void extraReloadConfig();
 
     //TODO: Parameters
     /**
@@ -38,11 +55,11 @@ public abstract class Configuration {
     /**
      * Sets the object to it value or default value.
      * @param chunk Chunk from which to extract the object, key and default value.
-     * @param <T> Type of chunk.
+     * @param <K> Type of chunk.
      */
-    public abstract <T> void setObjectFromChunk(ConfigChunk<T> chunk);
+    public abstract <K> void setObjectFromChunk(ConfigChunk<K> chunk);
 
-    public abstract <T> boolean doesChunkExist(ConfigChunk<T> chunk);
+    public abstract <K> boolean doesChunkExist(ConfigChunk<K> chunk);
 
     /**
      * Checks if a file exists
@@ -58,4 +75,10 @@ public abstract class Configuration {
             setObjectFromChunk(configChunk);
         }
     }
+
+    public <K> void addToConfigChunks(ConfigChunk<K> chunk) {
+        configChunks.add(chunk);
+    }
+
+    public abstract void backupConfigFile();
 }
--- src/main/java/dev/chechu/dragonapi/spigot/SpigotConfig.java
+++ src/main/java/dev/chechu/dragonapi/spigot/SpigotConfig.java
@@ -8,37 +8,38 @@ import lombok.Getter;
 import lombok.Setter;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.util.FileUtil;
 
 import java.io.File;
+import java.util.logging.Logger;
 
-public abstract class SpigotConfig extends Configuration {
-    @Getter private final JavaPlugin plugin;
-    @Getter(AccessLevel.PROTECTED) @Setter(AccessLevel.PROTECTED) private FileConfiguration config;
+public abstract class SpigotConfig extends Configuration<JavaPlugin> {
+    @Getter(AccessLevel.PROTECTED) private FileConfiguration config;
 
-
-    public SpigotConfig(JavaPlugin plugin) {
-        this.plugin = plugin;
-        setLogger(plugin.getLogger());
+    public SpigotConfig(Logger logger, JavaPlugin plugin) {
+        super(logger, plugin);
         config = plugin.getConfig();
-        createFiles();
-        if(fixVersions()) getLogger().info(Message.get("config_fix"));
     }
 
     @Override
     public void createFiles() {
-        if (!plugin.getDataFolder().exists()) if(!plugin.getDataFolder().mkdir()) {
+        if (!getPlugin().getDataFolder().exists()) if(!getPlugin().getDataFolder().mkdir()) {
             getLogger().severe(Message.get("config_folder_creation_error"));
         }
-        plugin.saveDefaultConfig();
-        File configFile = new File(plugin.getDataFolder() + "config.yml");
+        getPlugin().saveDefaultConfig();
+        File configFile = new File(getPlugin().getDataFolder() + "config.yml");
         if (!configFile.exists()) getLogger().severe(Message.get("config_file_creation_error"));
     }
 
     @Override
     public boolean fixVersions() {
+        boolean backedUp = false;
         boolean fixed = false;
         for (ConfigChunk<?> configChunk : getConfigChunks()) {
             if (configChunk.isFixable()) {
+                if(!backedUp) {
+                    backupConfigFile();
+                }
                 if (!doesChunkExist(configChunk)) {
                     config.set(configChunk.getKey(), config.get(configChunk.getOldKey(),configChunk.getDefaultValue()));
                     fixed = true;
@@ -51,8 +52,13 @@ public abstract class SpigotConfig extends Configuration {
             setObjectFromChunk(configChunk);
         }
 
-        if(fixed) plugin.saveConfig();
+        if(fixed) getPlugin().saveConfig();
         return fixed;
+    }
+
+    @Override
+    public void backupConfigFile() {
+        FileUtil.copy(new File(getPlugin().getDataFolder(), "config.yml"), new File(getPlugin().getDataFolder(), "config.yml.backup"));
     }
 
     @Override
@@ -64,5 +70,11 @@ public abstract class SpigotConfig extends Configuration {
     public <T> boolean doesChunkExist(ConfigChunk<T> chunk) {
         chunk.setValue(config.getObject(chunk.getKey(), chunk.getType(),null));
         return chunk.getValue() != null;
+    }
+
+    @Override
+    public void finishReloadConfig() {
+        getPlugin().reloadConfig();
+        config = getPlugin().getConfig();
     }
 }
