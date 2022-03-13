231a0c0ac302c5872e105e7ddcfd4ac3077aadd0
Chechu <22799163+DarkDragon117@users.noreply.github.com>
2022-03-13 01:09:20 +0100

    Removed Config Test since it needs to be rewritten.

--- src/test/java/dev/chechu/core/ConfigTest.java
+++ src/test/java/dev/chechu/core/ConfigTest.java
@@ -9,37 +9,6 @@ import java.io.File;
 public class ConfigTest {
     @Test
     public void assertThatFileDoesntExist() {
-        Configuration configuration = new Configuration() {
-            @Override
-            public void initializeConfig() {
 
-            }
-
-            @Override
-            public void reloadConfig() {
-
-            }
-
-            @Override
-            public void createFiles() {
-
-            }
-
-            @Override
-            public boolean fixVersions() {
-                return false;
-            }
-
-            @Override
-            public <T> void setObjectFromChunk(ConfigChunk<T> chunk) {
-
-            }
-
-            @Override
-            public <T> boolean doesChunkExist(ConfigChunk<T> chunk) {
-                return false;
-            }
-        };
-        configuration.checkFile(new File("a"));
     }
 }
